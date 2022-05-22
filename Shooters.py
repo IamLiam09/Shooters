ab49e8e6d9bff43ee85c86408b2efc965e09cbe8
liams <princewill835@gmail.com>
2022-05-22 06:35:27 -0700

    adding images

--- Shooters.py
+++ Shooters.py
@@ -1,8 +1,8 @@
 import pygame
-screen_size = [500, 800]
+screen_size = [500, 400]
 screen = pygame.display.set_mode(screen_size)
-background = pygame.image.load()
-
+background = pygame.image.load('img/background.png')
+planet = pygame.image.load()
 # loop for the game
 keep_alive = True
 while keep_alive:
